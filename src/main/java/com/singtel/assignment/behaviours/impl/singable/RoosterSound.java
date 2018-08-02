a76f80ee74a9621f2ba48d7b3328633e45c45e92
aarthishuba <aarthishuba@gmail.com>
2018-08-02 12:14:22 +0800

    Coding completed for Counting animals and  i18n support implemeted

--- src/main/java/com/singtel/assignment/behaviours/impl/singable/RoosterSound.java
+++ src/main/java/com/singtel/assignment/behaviours/impl/singable/RoosterSound.java
@@ -2,10 +2,10 @@ package com.singtel.assignment.behaviours.impl.singable;
 
 import com.singtel.assignment.behaviours.Singable;
 
-public class RoosterSound implements Singable {
+public class RoosterSound extends CanSing {
 
     @Override
     public void sing() {
-        System.out.println("Cock-a-doodle-doo");
+        System.out.println(getSound("rooster"));
     }
 }
